f2984b5f4c576ed6c262e5f872a13da1898ca535
JIUNKOO <52573800+jiunkoo@users.noreply.github.com>
2025-11-03 20:46:26 +0900

    refactor: Home 및 MyPage 페이지 BEM 스타일 및 디자인 토큰 적용
    - Home: BEM 클래스명, CSS 변수 적용, React.FC 제거, 로딩 UI 추가
    - MyPage: BEM 클래스명, CSS 변수 적용, React.FC 제거

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,149 +1,148 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import Icon from "@components/icons/SvgIcon";
-import "@styles/mypage.css";
-import Layout from "@components/Layout";
-import { MYPAGE_CONSTANTS } from "@constants";
 
-const MyPage: React.FC = () => {
+import { Icon, Layout } from "@components";
+import { MYPAGE_CONSTANTS } from "@constants";
+import "@styles/mypage.css";
+
+const MyPage = () => {
   return (
     <Layout showMyPageSidebar={true}>
-      <div className="mypage-container">
-        <div className="mypage-grade-section">
-          <div className="mypage-grade-item mypage-grade-item-standard">
-            <div className="mypage-grade-header">
-              <div className="mypage-grade-label">
+      <main className="mypage">
+        <div className="mypage__grade-section">
+          <div className="mypage__grade-item mypage__grade-item--standard">
+            <div className="mypage__grade-header">
+              <div className="mypage__grade-label">
                 {MYPAGE_CONSTANTS.GRADE.LABEL}
               </div>
               <Icon
                 name="arrowRight"
                 size={20}
-                className="mypage-grade-header-arrow"
+                className="mypage__grade-header-arrow"
                 color="white"
               />
             </div>
-            <div className="mypage-grade-footer">
-              <div className="mypage-grade-value">
+            <div className="mypage__grade-footer">
+              <div className="mypage__grade-value">
                 {MYPAGE_CONSTANTS.GRADE.VALUE}
               </div>
-              <div className="mypage-grade-benefit-button">
+              <button type="button" className="mypage__grade-benefit-button">
                 {MYPAGE_CONSTANTS.GRADE.BENEFIT_BUTTON}
-              </div>
+              </button>
             </div>
           </div>
-          <div className="mypage-grade-item mypage-grade-item-standard">
-            <div className="mypage-grade-header">
-              <div className="mypage-grade-label">
+          <div className="mypage__grade-item mypage__grade-item--standard">
+            <div className="mypage__grade-header">
+              <div className="mypage__grade-label">
                 {MYPAGE_CONSTANTS.GRADE.COUPON_LABEL}
               </div>
               <Icon
                 name="arrowRight"
                 size={20}
-                className="mypage-grade-header-arrow"
+                className="mypage__grade-header-arrow"
                 color="white"
               />
             </div>
-            <div className="mypage-grade-footer">
-              <div className="mypage-grade-value">4</div>
+            <div className="mypage__grade-footer">
+              <div className="mypage__grade-value">4</div>
             </div>
           </div>
-          <div className="mypage-grade-item mypage-grade-item-standard">
-            <div className="mypage-grade-header">
-              <div className="mypage-grade-label">
+          <div className="mypage__grade-item mypage__grade-item--standard">
+            <div className="mypage__grade-header">
+              <div className="mypage__grade-label">
                 {MYPAGE_CONSTANTS.GRADE.MILEAGE_LABEL}
               </div>
               <Icon
                 name="arrowRight"
                 size={20}
-                className="mypage-grade-header-arrow"
+                className="mypage__grade-header-arrow"
                 color="white"
               />
             </div>
-            <div className="mypage-grade-footer">
-              <div className="mypage-grade-value">0</div>
+            <div className="mypage__grade-footer">
+              <div className="mypage__grade-value">0</div>
             </div>
           </div>
         </div>
 
-        <div className="mypage-section">
-          <div className="mypage-section-header">
-            <div className="mypage-section-title">
+        <div className="mypage__section">
+          <div className="mypage__section-header">
+            <div className="mypage__section-title">
               {MYPAGE_CONSTANTS.SECTION.RECENT_ORDER}
             </div>
-            <Link to="/orderlist" className="mypage-section-more">
-              <div className="mypage-section-more-text">
+            <Link to="/orderlist" className="mypage__section-more">
+              <span className="mypage__section-more-text">
                 {MYPAGE_CONSTANTS.SECTION.MORE}
-              </div>
+              </span>
               <Icon
                 name="arrowRight"
                 size={16}
-                className="mypage-section-more-arrow"
+                className="mypage__section-more-arrow"
                 color="black"
               />
             </Link>
           </div>
 
-          <div className="mypage-order-item">
-            <div className="mypage-order-date">
+          <div className="mypage__order-item">
+            <div className="mypage__order-date">
               {MYPAGE_CONSTANTS.ORDER.DATE}
             </div>
-            <div className="mypage-order-image">
+            <div className="mypage__order-image">
               <Icon name="image" size={40} />
             </div>
-            <div className="mypage-order-info">
-              <div className="mypage-order-product">
+            <div className="mypage__order-info">
+              <div className="mypage__order-product">
                 {MYPAGE_CONSTANTS.ORDER.PRODUCT}
               </div>
             </div>
-            <div className="mypage-order-price">
+            <div className="mypage__order-price">
               {MYPAGE_CONSTANTS.ORDER.PRICE}
             </div>
           </div>
         </div>
 
-        <div className="mypage-section">
-          <div className="mypage-section-header">
-            <div className="mypage-section-title">
+        <div className="mypage__section">
+          <div className="mypage__section-header">
+            <div className="mypage__section-title">
               {MYPAGE_CONSTANTS.SECTION.WISHLIST}
             </div>
-            <div className="mypage-section-more">
-              <div className="mypage-section-more-text">
+            <div className="mypage__section-more">
+              <span className="mypage__section-more-text">
                 {MYPAGE_CONSTANTS.SECTION.MORE}
-              </div>
+              </span>
               <Icon
                 name="arrowRight"
                 size={16}
-                className="mypage-section-more-arrow"
+                className="mypage__section-more-arrow"
                 color="black"
               />
             </div>
           </div>
 
-          <div className="mypage-wishlist-tab-item">
+          <div className="mypage__wishlist-tab-item">
             {MYPAGE_CONSTANTS.WISHLIST.PRODUCT_TAB}
           </div>
 
-          <div className="mypage-wishlist-grid">
+          <div className="mypage__wishlist-grid">
             {[1, 2, 3, 4, 5, 6].map((item) => (
-              <div key={item} className="mypage-wishlist-item">
-                <div className="mypage-wishlist-image">
+              <div key={item} className="mypage__wishlist-item">
+                <div className="mypage__wishlist-image">
                   <Icon name="image" size={60} />
                 </div>
               </div>
             ))}
           </div>
 
-          <div className="mypage-wishlist-tab-item">
+          <div className="mypage__wishlist-tab-item">
             {MYPAGE_CONSTANTS.SECTION.BRAND}
           </div>
 
-          <div className="mypage-wishlist-item">
-            <div className="mypage-wishlist-item-empty-text">
+          <div className="mypage__wishlist-item">
+            <div className="mypage__wishlist-item-empty-text">
               {MYPAGE_CONSTANTS.WISHLIST.EMPTY_BRAND}
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </Layout>
   );
 };
